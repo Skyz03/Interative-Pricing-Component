cdfac7ae130528c3ccc007ac3d820eec5a892aa6
Syz03 <aakibdiscoo@gmail.com>
2021-09-08 20:34:32 +0545

    Updates in the JS

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function updateValue(val) {
     document.getElementById('range').value = val;
 
-    document.getElementById('dollar').innerHTML = "$" + val + ".00";
+    return document.getElementById('dollar').innerHTML = "$" + val + ".00";
+}
+
+function updateCheck(value) {
+    let checkVal = document.querySelector('#check').checked
+
+    if (checkVal == true) {
+        document.getElementById('per-month').innerHTML = updateValue(value) + "/year"
+    }
 }
