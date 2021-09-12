4b4138d1d551b51e6c3a2ac5da59e266fbcf68f0
Syz03 <aakibdiscoo@gmail.com>
2021-09-12 22:47:11 +0545

    JS added

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+import { viewsPrice } from "./priceOutput.js";
+
+export const SLIDER = document.querySelector(".slider");
+const FOCUS_ME = document.querySelector("#focusMe");
+
+SLIDER.addEventListener("mouseout", () => {
+    FOCUS_ME.focus();
+});
+
+// calculate the width of the background-image based on the value of the input range
+const setWidth = () => {
+    return SLIDER.value * 25 + "%";
+};
+
+// when the input range is changed, calculate the size of the background-image and evaluate what the pageviews and price text should be
+SLIDER.addEventListener("input", () => {
+    SLIDER.style.backgroundSize = setWidth() + " 100%";
+    viewsPrice();
+});
